dbf7c68179fadce8f2c06847a680c3abb39797d7
darknet <nkrezeski1@gmail.com>
2021-06-19 01:34:57 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+With this python script you can download youtube videos in m4a format.
+
+Features:
+1. Download a single song
+2. Download a whole playlist
+3. Download a whole album
+
+You can either download in:
+1. Default location(C:\Music)
+2. Custom location(You can input custom absolute path)
+
+To be able to start using this script, you have to first edit line 7 with your youtube data api key, you can read more about it here: https://developers.google.com/youtube/v3/getting-started
---
+++ yt_download.py
@@ -0,0 +1,92 @@
+import pafy
+import os
+import sys
+import subprocess
+import platform
+
+# Set yt_api_key
+api_key = "Add your api key here"
+pafy.set_api_key(api_key)
+
+# Install dependency packages function
+def install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+# Handle download location
+def down_location(type, playlist_name=""):
+    def_loc_line = None
+    if type == 1:
+        def_loc_line = "\Single songs"
+    elif type == 2:
+        def_loc_line = "\Playlists"
+    elif type == 3:
+        def_loc_line = "\Albums"
+
+    print("Use default download location or a custom one? (Just type 1 or 2)")
+    print("1. Default location")
+    print("2. Custom location")
+    location = int(input())
+    if location == 1:
+        current_os = platform.system()
+        if current_os == "Windows":
+            os.mkdir("C:\Music")
+            os.mkdir("C:\Music" + def_loc_line)
+            os.chdir("C:\Music" + def_loc_line)
+        else:
+            os.mkdir("/home/Music")
+            os.mkdir("/home/Music" + def_loc_line)
+            os.chdir("/home/Music" + def_loc_line)
+        if playlist_name != "":
+            if not os.path.exists(playlist_name):
+                os.mkdir(playlist_name)
+            os.chdir(playlist_name)
+    elif location == 2:
+        custom_dir = input("Enter absolute path of custom directory \n")
+        os.chdir(custom_dir)
+    else:
+        print("Invalid input")
+
+def down_playlist(down_type):
+    url = input("Enter playlist url: \n")
+    playlist = pafy.get_playlist2(url)
+    down_location(down_type, playlist.title)
+
+    curr_song = 1
+    for song in playlist:
+        try:
+            print(
+                f"Currently downloading song {curr_song}/{len(playlist) + 1} - [{int((curr_song / len(playlist)) * 100)} %]")
+            song_video = pafy.new(song.watchv_url)
+            audiostreams = song_video.audiostreams
+            audiostreams[2].download(song_video.title + ".m4a")
+            curr_song += 1
+        except:
+            pass
+
+
+if __name__ == '__main__':
+    # Install pafy package
+    install('pafy')
+
+    # Main
+    print("Choose what type of download you're going to use: (Just type 1, 2 or 3)")
+    print("1. A single song")
+    print("2. A playlist")
+    print("3. An album")
+    down_type = int(input())
+
+    if down_type == 1:
+        url = input("Enter video url: \n")
+        video = pafy.new(url)
+        down_location(down_type)
+        audiostreams = video.audiostreams
+        audiostreams[2].download(video.title + ".m4a")
+
+    elif down_type == 2:
+        down_playlist(down_type)
+
+    elif down_type == 3:
+        down_playlist(down_type)
+
+    else:
+        print("Invalid input")
